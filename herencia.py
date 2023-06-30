9a45a193aa6953e85ab3380724612ea2907b9dc5
anibalkozur <111557307+anibalkozur@users.noreply.github.com>
2023-06-30 20:36:32 -0300

    Update herencia.py

--- herencia.py
+++ herencia.py
@@ -1,3 +1,9 @@
-#hola
-#sdfasdfad
-#adaafsdfasdf
+#gerarquia de herencia
+#1-Clase padre superclase
+#2-Subclase
+#3-Clases
+
+#sirve para la reutilizacion del codigo
+#en caso de objetos similares
+#con caracteristicas comunes y
+#comportamientos en comun
