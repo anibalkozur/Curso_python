94ae12fb2c5ab18d603c5653d48ba99636ad7a56
anibalkozur <111557307+anibalkozur@users.noreply.github.com>
2023-07-03 18:52:08 -0300

    Create uso_funciones.py

---
+++ Modulos/uso_funciones.py
@@ -0,0 +1,17 @@
+#importa el modulo nombrado
+#import funciones_matematicas
+#se utiliza la nomenclatura del punto
+
+#esto se hace para importar funciones pero restringe
+#el uso de las que no se importa
+#from funciones_matematicas import sumar, restar
+
+#se soluciona poniendo un asterisco
+from funciones_matematicas import *
+
+#funciones_matematicas.sumar(7,5)
+
+#funciones_matematicas.restar(12,5)
+
+sumar(7,5)
+restar(12,5)
