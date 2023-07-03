4bc6d5224d156db046685ec2ba484c420adb4000
anibalkozur <111557307+anibalkozur@users.noreply.github.com>
2023-07-03 18:51:39 -0300

    Create Modulos.py

---
+++ Modulos.py
@@ -0,0 +1,8 @@
+#archivo con extension .py .pyc(python compilado) o
+#archivo escrito en C para python
+#posee su propio espacio de nombres y que puede contener
+#variables, funciones, clases, e incluso otros modulos.
+
+#organizar y reutilizar nuestros modulos
+#(modulizacion y reutilizacion)
+#mas facil entendimiento del codigo
