5e88560d599659f7cf83a304bffbe93d0105f2dc
anibalkozur <111557307+anibalkozur@users.noreply.github.com>
2023-07-03 18:11:48 -0300

    Update cadena_caracteres.py

--- cadena_caracteres.py
+++ cadena_caracteres.py
@@ -1,6 +1,18 @@
 #String
 #Metodos:
 
+#upper(): convierte en mayusculas todas las letras de un String
+#lower(): pasa a minuscula un String
+#capitalize(): pone la primera letra en mayuscula
+#count(): contar cuantas veces aparece una letra o cadena de caracateres
+#find():posicion en la que aparece un caracter o cadena de caracteres
+#isdigit(): devuelve un boleano si es numerico o no el valor
+#isalum(): comprobar si es alfanumerico
+#isalpha(): si hay solo letras, los espacios no cuentan
+#split(): separa por palabras utilizando espacios
+#strip(): borra los espacios sobrantes al principio y al final
+#remplace(): cambia una un caracter o cadena por otra dentro del string
+#rfind(): como find pero desde atras, posicion en la que aparece un caracter o cadena de caracteres
 
-
+#hay mas
 
