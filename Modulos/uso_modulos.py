b0ef5ad2b1b3e3b65aca1971cb03d1142c0d54b8
anibalkozur <111557307+anibalkozur@users.noreply.github.com>
2023-07-04 09:10:10 -0300

    Create uso_modulos.py

---
+++ Modulos/uso_modulos.py
@@ -0,0 +1,9 @@
+#para usar el modulo claculos_genrales de la carpeta calculos
+#se coloca toda la ruta
+
+from calculos.basicos.operaciones_basicas import *
+from calculos.redondeo_potencia.redondeo_potencia import *
+#ó from calculos.calculos_generales import dividir, potencia
+
+dividir(4,6)
+potencia(2,6)
