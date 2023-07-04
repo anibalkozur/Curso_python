545aad4b4a0c49bfc7dafe91fa33afa4a4d0e35e
anibalkozur <111557307+anibalkozur@users.noreply.github.com>
2023-07-04 09:10:20 -0300

    Create paquetes.py

---
+++ Modulos/paquetes.py
@@ -0,0 +1,7 @@
+#Paquetes:
+# directorios donde se almacenaran modulos entre si
+# modulos relacio0nados entre si.
+#organiza el codigo y reutilizar
+
+#se crea haciendo una carpeta y para que esta funcione
+#como paquete debe contener un archivo __init__.py
