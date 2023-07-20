75933e5c3cf0a8dc08301c476160e4dc8c4b7d45
anibalkozur <111557307+anibalkozur@users.noreply.github.com>
2023-07-20 18:05:32 -0300

    Create setup.py

---
+++ Modulos/calculos/redondeo_potencia/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+
+    name="paquetecalculos",#cualquier nombre
+    version="1.0",#cualquier numero
+    description="Paquete de redondeo y potencia",
+    author="Kozur Anibal Martin",
+    author_email="anibalkozur@hotmaiul.com",
+    #url="direccion de pagina"
+    #packages=["redondeo_potencia"]
+    #en consola de cimando cmd se pone
+    #python setup.py sdist
+    #para que sea una paquete distribuible
+)
