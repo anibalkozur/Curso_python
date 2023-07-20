0b021744d011d9274a0d6167789b4508a38b60e9
anibalkozur <111557307+anibalkozur@users.noreply.github.com>
2023-07-20 19:18:17 -0300

    Create Primera_interface.py

---
+++ Graficos/Primera_interface.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+#se cra un objeto ventana
+raiz=Tk()
+
+raiz.title("Ventana de Ian")
+
+raiz.iconbitmap("C:/Users/Aniba/OneDrive/Escritorio/estudio programacion/Curso_python/Graficos/linian logo cuadrado.ico")
+
+#bloquea el tamaño de ventana (width "ancho", height "alto")
+#raiz.resizable(1,0)#true-false
+#es necesario para que se visualice la ventana en un bucle infinito
+raiz.mainloop()
