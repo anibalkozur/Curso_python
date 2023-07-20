a129d19de7d9bc713ffc3e37cad8da21f03a7433
anibalkozur <111557307+anibalkozur@users.noreply.github.com>
2023-07-20 19:27:03 -0300

    Update Primera_interface.py

--- Graficos/Primera_interface.py
+++ Graficos/Primera_interface.py
@@ -4,10 +4,17 @@ from tkinter import *
 raiz=Tk()
 
 raiz.title("Ventana de Ian")
-
+#cambiar el icono
 raiz.iconbitmap("C:/Users/Aniba/OneDrive/Escritorio/estudio programacion/Curso_python/Graficos/linian logo cuadrado.ico")
-
 #bloquea el tamaño de ventana (width "ancho", height "alto")
 #raiz.resizable(1,0)#true-false
+
+raiz.geometry("650x350")
+
+raiz.config(bg="blue")
+
 #es necesario para que se visualice la ventana en un bucle infinito
 raiz.mainloop()
+
+#para que funcione la ventana sin la consola
+#se cambia la extencion .py por pyw
